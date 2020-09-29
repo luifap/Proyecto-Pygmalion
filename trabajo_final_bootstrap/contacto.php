473e0b7c162842a0fab0ca9161ffc6e568387d21
luifap <52289372+luifap@users.noreply.github.com>
2020-09-29 16:40:56 -0500

    Actualización de Proyecto final

--- trabajo_final_bootstrap/contacto.php
+++ trabajo_final_bootstrap/contacto.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,9 +27,9 @@
 
     <div class="collapse navbar-collapse" id="collpasibleNavar">
       <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" href="registro.html">Conviértete en afiliado</a>
-        </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contacto.php">Conviértete en afiliado</a>
+      </li>
         <li class="nav-item">
           <a class="nav-link" href="iniciosecc.html">Su cuenta</a>
         </li>
@@ -56,29 +57,73 @@
       <hr class=" d-sm-nome">
     </div>
     <div class="col-sm-10">
+    <?php
+      include('conexion.php');
+      if((isset($_POST['name']) && !empty($_POST['name']))
+      && (isset($_POST['email']) && !empty($_POST['email']))
+      && (isset($_POST['telefono']) && !empty($_POST['telefono']))
+      && (isset($_POST['password']) && !empty($_POST['password']))){
+
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $telefono = $_POST['telefono'];
+        $password = $_POST['password'];
+
+        
+        
+
+        $query = "INSERT INTO `contacto` (name, email, telefono, password) VALUES ('$name', '$email', '$telefono', '$password')";
+          $result = mysqli_query($connection, $query);
+          echo "<center>Tú mensaje fue enviado con exito </br nos pondremos en contacto con usted pronto.</center>";
+        
+      }
+      ?>
       <div class="row">
         <div class="container-fluid">
           <div  class="container py-5">
-            <form>
-              <div class="form-group">
-                <label for="email">Nombre Completo:</label>
-                <input type="text" class="form-control" placeholder="Nombre Completo" id="name">
+					<form class="form-contact" method="POST">
+						<h2 class="form-contact-heading">Ingresa tus datos para registrarte</h2>
+						<div class="form-group">
+            <label for="name">Nombre completo:</label>
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text bg-light"><i class="fas fa-user"></i></span>
+                </div>
+                <input type="text" class="form-control" placeholder="Su nombre y apellidos" id="inputName" name="name" required>
               </div>
-              <div class="form-group">
-                <label for="email">Email address:</label>
-                <input type="email" class="form-control" placeholder="Enter email" id="email">
+            </div>
+
+            <div class="form-group">
+              <label for="email">Correo electrónico:</label>
+              <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text bg-light"><i class="fas fa-envelope"></i></span>
               </div>
-              <div class="form-group">
-                <label for="pwd">Password:</label>
-                <input type="password" class="form-control" placeholder="Enter password" id="pwd">
+              <input type="email" class="form-control" placeholder="name@email.com" id="inputEmail" name="email" required>
+              </div>    
+            </div>
+
+            <div class="form-group">
+            <label for="inputTelefono">Telefono</label>
+              <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text bg-light"><i class="fas fa-mobile-alt"></i></span>
               </div>
-              <div class="form-group form-check">
-                <label class="form-check-label">
-                  <input class="form-check-input" type="checkbox"> Remember me
-                </label>
+              <input type="telefono" class="form-control" placeholder="Teléfono" id="inputTelefono" name="telefono" required>
+              </div>    
+            </div>
+						
+            <div class="form-group">
+              <label for="pwd">Contraseña:</label>
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text bg-light"><i class="fas fa-unlock-alt"></i></span>
+                </div>
+                <input type="password" class="form-control" placeholder="Tú contraseña" id="pwd" name="password" required>
               </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+            </div>
+						<button class="btn btn-lg btn-primary btn-block" type="submit">Enviar Formulario</button>
+     			</form>
           </div>
         </div>
       </div>
@@ -86,7 +131,7 @@
   </div>
 </div>
   <!-- Footer -->
-<footer class="page-footer font-small unique-color-dark bg-dark">
+  <footer class="page-footer font-small unique-color-dark bg-dark">
   <!-- Footer Links -->
   <div class="container text-center text-md-left mt-5">
     <!-- Grid row -->
@@ -110,25 +155,6 @@
         <h6 class=" font-weight-bold">Servicios</h6>
         <hr class="bg-warning accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
         <p>
-          <a href="#!" class="text-white">Restaurante Bar</a>
-        </p>
-        <p>
-          <a href="#!" class="text-white">Gastronomía</a>
-        </p>
-        <p>
-          <a href="#!" class="text-white">Fechas especiales</a>
-        </p>
-
-      </div>
-      <!-- Grid column -->
-
-      <!-- Grid column -->
-      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 text-white">
-
-        <!-- Links -->
-        <h6 class=" font-weight-bold">Servicios</h6>
-        <hr class="bg-warning accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-        <p>
           <a href="galeria.html" class="text-white">Galería</a>
         </p>
         <p>
@@ -138,6 +164,21 @@
           <a href="fechas.html" class="text-white">Fechas especiales</a>
         </p>
 
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 text-white">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">ENLACES ÚTILES</h6>
+        <hr class="bg-warning accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <a href="iniciosecc.html" class="text-white">Su cuenta</a>
+        </p>
+        <p>
+          <a href="contacto.php" class="text-white">Conviértete en afiliado</a>
+        </p>
       </div>
       <!-- Grid column -->
 
